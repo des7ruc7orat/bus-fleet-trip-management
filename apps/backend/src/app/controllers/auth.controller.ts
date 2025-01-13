79b16ff1f8f04ef1ccf4bc141f3f7862e5b9a0ac
des7ruc7or <dimitar.dimitrov@deltastone.com>
2025-01-13 12:39:18 +0100

    making the AuthController

---
+++ apps/backend/src/app/controllers/auth.controller.ts
@@ -0,0 +1,17 @@
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('login')
+  async login(@Body() loginDto: { email: string; password: string }) {
+    const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+    return this.authService.login(user);
+  }
+}
