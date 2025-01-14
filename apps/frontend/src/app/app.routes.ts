7008c37f04d958fb34c3c15f283fa9e79e5f839a
des7ruc7or <dimitar.dimitrov@deltastone.com>
2025-01-14 22:00:57 +0100

    api routes got the login and the bus form

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { LoginComponent } from './components/login/ui/login.component';
+import { BusFormComponent } from './components/bus/ui/form/bus-form.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'login', component: LoginComponent },
+  { path: 'bus-form', component: BusFormComponent },
+];
